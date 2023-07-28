0b7ef537b789ea3220e944c05e31d3eb1393bb24
Ilja Jusupov <illvdg13@gmail.com>
2023-07-28 16:26:14 -0300

    Update plugin-main.c

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,3 +1,11 @@
+/*
+# Copyright (C) [year] [name of author]
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+*/
+
 #pragma warning(push)
 #pragma warning(disable : 4005)
 #define WIN32_LEAN_AND_MEAN
